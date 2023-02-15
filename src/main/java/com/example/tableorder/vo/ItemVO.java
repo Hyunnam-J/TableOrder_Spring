dabe72e12b057e047f2f81e0af153890ea11673c
Hyunnam-J <nami0879@naver.com>
2023-02-15 17:34:47 +0900

    main

---
+++ src/main/java/com/example/tableorder/vo/ItemVO.java
@@ -0,0 +1,71 @@
+package com.example.tableorder.vo;
+
+public class ItemVO {
+		
+	String comId, pName, itemName2, pos, pCode, tabNo, itemCode, UPrice;
+
+	public String getComId() {
+		return comId;
+	}
+
+	public void setComId(String comId) {
+		this.comId = comId;
+	}
+
+	public String getpName() {
+		return pName;
+	}
+
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	public String getItemName2() {
+		return itemName2;
+	}
+
+	public void setItemName2(String itemName2) {
+		this.itemName2 = itemName2;
+	}
+
+	public String getPos() {
+		return pos;
+	}
+
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
+
+	public String getpCode() {
+		return pCode;
+	}
+
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+
+	public String getTabNo() {
+		return tabNo;
+	}
+
+	public void setTabNo(String tabNo) {
+		this.tabNo = tabNo;
+	}
+
+	public String getItemCode() {
+		return itemCode;
+	}
+
+	public void setItemCode(String itemCode) {
+		this.itemCode = itemCode;
+	}
+
+	public String getUPrice() {
+		return UPrice;
+	}
+
+	public void setUPrice(String uPrice) {
+		UPrice = uPrice;
+	}
+	
+}
---
+++ src/main/java/com/example/tableorder/vo/TabCodeVO.java
@@ -0,0 +1,103 @@
+package com.example.tableorder.vo;
+
+public class TabCodeVO {
+		
+	String comId, pName, pos, pCode, tabNo, mon, tue, wed, thu, fri, sat, sun;
+
+	public String getComId() {
+		return comId;
+	}
+
+	public void setComId(String comId) {
+		this.comId = comId;
+	}
+
+	public String getpName() {
+		return pName;
+	}
+
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	public String getPos() {
+		return pos;
+	}
+
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
+
+	public String getpCode() {
+		return pCode;
+	}
+
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+
+	public String getTabNo() {
+		return tabNo;
+	}
+
+	public void setTabNo(String tabNo) {
+		this.tabNo = tabNo;
+	}
+
+	public String getMon() {
+		return mon;
+	}
+
+	public void setMon(String mon) {
+		this.mon = mon;
+	}
+
+	public String getTue() {
+		return tue;
+	}
+
+	public void setTue(String tue) {
+		this.tue = tue;
+	}
+
+	public String getWed() {
+		return wed;
+	}
+
+	public void setWed(String wed) {
+		this.wed = wed;
+	}
+
+	public String getThu() {
+		return thu;
+	}
+
+	public void setThu(String thu) {
+		this.thu = thu;
+	}
+
+	public String getFri() {
+		return fri;
+	}
+
+	public void setFri(String fri) {
+		this.fri = fri;
+	}
+
+	public String getSat() {
+		return sat;
+	}
+
+	public void setSat(String sat) {
+		this.sat = sat;
+	}
+
+	public String getSun() {
+		return sun;
+	}
+
+	public void setSun(String sun) {
+		this.sun = sun;
+	}
+			
+}
